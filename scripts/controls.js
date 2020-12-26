e750394eb4f92ac91f96bc0fb79551ec8bfc3d0e
Santiago Lopera <santiagoln09@gmail.com>
2020-12-26 10:33:21 -0500

    RPS

--- scripts/controls.js
+++ scripts/controls.js
@@ -8,6 +8,26 @@ for (let i = 0; i < iconSelected.length; i++) {
   });
 }
 
+
+function addPoints(user,cloud){
+  let userContainer = document.querySelector('#points1');
+  let cloudContainer = document.querySelector('#points2');
+
+  let userPoints = 0;
+  let cloudPoints = 0;
+  if(user && cloud){
+    userPoints = userPoints + 1;
+    cloudPoints = cloudPoints + 1;
+  }else if(cloud){
+    cloudPoints = cloudPoints + 1;
+  }else if(user){
+    userPoints = userPoints + 1;
+  }
+  userContainer.innerHTML = userPoints;
+  cloudContainer.innerHTML = cloudPoints;
+  
+}
+
 function userSelecction(numberIcon) {
   /* 0 -> scissors
      1 -> paper
@@ -21,31 +41,94 @@ function userSelecction(numberIcon) {
   }
   switch (numberIcon) {
     case 0:
-      user = "Tijeras";
+      user = "scissors";
       break;
     case 1:
-      user = "Papel";
+      user = "paper";
       break;
     case 2:
-      user = "Piedra";
+      user = "rock";
       break;
     case 3:
       let random = parseInt(Math.random() * (3 - 0) + 0);
       switch (random) {
         case 0:
-          user = "Tijeras";
+          user = "scissors";
           break;
         case 1:
-          user = "Papel";
+          user = "paper";
           break;
         case 2:
-          user = "Piedra";
+          user = "rock";
           break;
       }
       break;
   }
   console.log(user);
+  game(user);
 }
+
+function cloudChoice() {
+  let RandomNumber = parseInt(Math.random() * 3) + 1;
+  let cloud = "";
+  switch (RandomNumber) {
+    case 1:
+      cloud = "scissors";
+      return cloud;
+    case 2:
+      cloud = "paper";
+      return cloud;
+
+    case 3:
+      cloud = "rock";
+      return cloud;
+  }
+}
+
+function game(user) {
+  let cloud = cloudChoice();
+  console.log("La nube saca = " + cloud);
+  switch (user) {
+    case "rock" /* Si saco piedra */:
+      if (user == cloud) {
+        console.log("Empate!");
+        addPoints(1,1);
+      } else if (cloud == "paper") {
+        console.log("Uy!, la nube saco papel. ¡Perdiste!");
+        addPoints(0,1);
+      } else if (cloud == "scissors") {
+        console.log("¡Ganaste!, la nube saco tijera");
+        addPoints(1,0);
+      }
+      break;
+    case "paper" /* Si saco papel */:
+      if (user == cloud) {
+        console.log("Empate!");
+        addPoints(1,1);
+      } else if (cloud == "scissors") {
+        console.log("Uy!, la nube saco tijera. ¡Perdiste!");
+        addPoints(0,1);
+      } else if (cloud == "rock") {
+        console.log("¡Ganaste!, la nube saco piedra");
+        addPoints(1,0);
+      }
+      break;
+    case "scissors" /* Si saco tijera */:
+      if (user == cloud) {
+        console.log("Empate!");
+        addPoints(1,1);
+      } else if (cloud == "rock") {
+        console.log("Uy!, la nube saco piedra. ¡Perdiste!");
+        addPoints(0,1);
+      } else if (cloud == "paper") {
+        console.log("¡Ganaste!, la nube saco papel");
+        addPoints(1,0);
+      }
+      break;
+  }
+}
+
+
 
 /* function MostrarNombre(){
    document.querySelector('#overflow').classList.add('overflowDown');
